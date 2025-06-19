aa8b81cc385b521ce1d849dcbf9e7f95d17dfe6b
Kumampet <kumampet@gmail.com>
2025-06-19 09:24:28 +0900

    build fix

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   output: "export",
-  // distDir: "./docs",
+  distDir: process.env.NODE_ENV === 'production' ? 'docs' : '.next',
   basePath: "/aqours6thcolor",
 };
 
